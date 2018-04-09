3bddf9cb1ae18b7db25f9daffa77964b18790f19
Jeroen Breen <>
2018-04-09 16:45:13 +0200

    replace by online files

---
+++ js/templates.js
@@ -0,0 +1,14 @@
+app.directive('tools', function() {
+    return {
+        restrict : 'E',
+        templateUrl : 'templates/tools.html'
+    };
+}); 
+
+
+app.directive('workarea', function() {
+    return {
+        restrict : 'E',
+        templateUrl : 'templates/workarea.html'
+    };
+}); 
