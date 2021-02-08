cbc296934ea3527019ee3985125acc553f0c603c
Jeroen Breen <>
2021-02-08 10:51:29 +0100

    Update App.js

--- fontapp/src/App.js
+++ fontapp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from "./components/about/About";
 import Main from "./components/main/Main";
 import './App.scss';
