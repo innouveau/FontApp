60f66449e1b0690f2be80e385aed86750fc04457
Jeroen Breen <>
2021-02-22 11:40:23 +0100

    initial files

---
+++ fontapp/src/components/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Tools from './tools/Tools.svelte'
+    export let name;
+</script>
+
+<main>
+    Main
+</main>
+
+<Tools/>
+
+<style>
+
+
+</style>
---
+++ fontapp/src/components/tools/Tools.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<div class="Tools">
+    Tools
+</div>
+
+<style>
+    .Tools {
+
+    }
+</style>
